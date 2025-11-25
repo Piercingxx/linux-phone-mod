8be61f6818563a34ed9de0711126f44a949d15e3
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-11-25 16:21:32 -0500

    Update droidian-mod.sh

--- droidian-mod.sh
+++ droidian-mod.sh
@@ -25,7 +25,7 @@ fi
 # Install required tools for TUI
 if ! command -v whiptail &> /dev/null; then
     echo -e "${YELLOW}Installing whiptail...${NC}"
-    apt install whiptail -y
+    sudo apt install whiptail -y
 fi
 
 username=$(id -u -n 1000)
@@ -43,10 +43,7 @@ function msg_box() {
 function menu() {
     whiptail --backtitle "GitHub.com/PiercingXX" --title "Main Menu" \
         --menu "Run Options In Order:" 0 0 0 \
-        "Step 1"                                "Install Depends" \
-        "Step 2"                                "Install Applications and Utilities" \
-        "PiercingXX Rice"                       "Apply Piercing Rice (Distro Agnostic)" \
-        "Piercing Gimp"                         "Piercing Gimp Presets (Distro Agnostic)" \
+        "Install"                               "Install PiercingXX Mod" \
         "Reboot System"                         "Reboot the system" \
         "Exit"                                  "Exit the script" 3>&1 1>&2 2>&3
 }
@@ -56,108 +53,45 @@ while true; do
     echo -e "${GREEN}Welcome ${username}${NC}\n"
     choice=$(menu)
     case $choice in
-        "Step 1")
-            sudo apt update && upgrade -y
-            wait
-            sudo apt full-upgrade -y
-            wait
-            sudo apt install -f
-            wait
-            sudo dpkg --configure -a
-            sudo apt --fix-broken install -y
-            wait
-            sudo apt autoremove -y
-            sudo apt update && upgrade -y
+        "Install")
             echo -e "${YELLOW}Updating System...${NC}"
-            # Check if nala is installed
-                if ! command_exists nala; then
-                echo "nala is not installed. Installing now..."
-                # Install nala using apt
-                sudo apt install nala -y
-                fi
-            wait
-            # Check if flatpak is installed and update it
-                if command_exists flatpak; then
-                    echo "Updating flatpak packages..."
-                    flatpak update -y
-                else
-                    echo "Flatpak is not installed."
-                    sudo apt install flatpak -y
-                    sudo apt install gnome-software-plugin-flatpak -y
-                    flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-                    flatpak update
-                fi
-            wait
-            echo -e "${GREEN}System Updated Successfully!${NC}"
-            echo -e "${YELLOW}Installing Core Dependencies...${NC}"
+            # Install Gnome and Dependencies
                 cd scripts || exit
                 chmod u+x step-1.sh
                 sudo ./step-1.sh
+                wait
                 cd "$builddir" || exit
-            echo -e "${GREEN}Core Dependencies Installed successfully!${NC}"
-            ;;
-        "Step 2")
-            echo -e "${YELLOW}Installing Core Applications...${NC}"
+            # Apply Piercing Rice
+                echo -e "${YELLOW}Applying PiercingXX Gnome Customizations...${NC}"
+                rm -rf piercing-dots
+                git clone --depth 1 https://github.com/Piercingxx/piercing-dots.git
+                cd piercing-dots || exit
+                chmod u+x install.sh
+                ./install.sh
+                wait
+                cd "$builddir" || exit
+            # Install Apps & Dependencies
+                echo -e "${YELLOW}Installing Apps & Dependencies...${NC}"
                 cd scripts || exit
                 chmod u+x apps.sh
                 sudo ./apps.sh
+                wait
                 cd "$builddir" || exit
-            echo -e "${GREEN}Core Apps Installed successfully!${NC}"
-            ;;
-        "PiercingXX Rice")
-            echo -e "${YELLOW}Downloading and Applying PiercingXX Rice...${NC}"
-                # .config Dot Files
-                echo -e "${YELLOW}Downloading PiercingXX Dot Files...${NC}"
-                    git clone https://github.com/Piercingxx/piercing-dots.git
-                        chmod -R u+x piercing-dots
-                        chown -R "$username":"$username" piercing-dots
-                        cd piercing-dots || exit
-                        cp -Rf dots/* /home/"$username"/.config/
-                        chown "$username":"$username" -R /home/"$username"/.config/*
-                        cd "$builddir" || exit      
-                    echo -e "${GREEN}PiercingXX Dot Files Applied Successfully!${NC}"
-                # Piercings Gnome Customizations
-                    echo -e "${YELLOW}Applying PiercingXX Gnome Customizations...${NC}"
-                        cd piercing-dots || exit
-                        cd scripts || exit
-                        ./pureos-customizations.sh
-                        cd "$builddir" || exit
-                # Add in backgrounds and themes and apply them
-                    mkdir -p /home/"$username"/Pictures/backgrounds
-                    chown -R "$username":"$username" /home/"$username"/Pictures/backgrounds
-                    cp -Rf piercing-dots/backgrounds/* /home/"$username"/Pictures/backgrounds
-                    chown -R "$username":"$username" /home/"$username"/Pictures/backgrounds
-                    mkdir -p /home/"$username"/Pictures/profile-image
-                    chown -R "$username":"$username" /home/"$username"/Pictures/profile-image
-                    cp -Rf piercing-dots/profile-image/* /home/"$username"/Pictures/profile-image
-                    chown -R "$username":"$username" /home/"$username"/Pictures/profile-image
-                    cd "$builddir" || exit
-                # Copy Refs to Download folder
-                    mkdir -p /home/"$username"/Downloads/refs
-                    chown -R "$username":"$username" /home/"$username"/Downloads/refs
-                    cp -Rf piercing-dots/refs/* /home/"$username"/Downloads/refs
-                    chown -R "$username":"$username" /home/"$username"/Downloads/refs
-                # Replace .bashrc
-                    cp -Rf piercing-dots/bash/.bashrc /home/"$username"/
-                    chown -R "$username":"$username" /home/"$username"/.bashrc
+            # Apply Piercing Gnome Customizations as User
+                cd piercing-dots/scripts || exit
+                ./gnome-customizations.sh
+                wait
+                cd "$builddir" || exit
+            # Replace .bashrc
+                cp -f piercing-dots/resources/bash/.bashrc /home/"$username"/.bashrc
+                source ~/.bashrc
+            # Bash Stuff
+                install_bashrc_support
+            # Clean Up
                 rm -rf piercing-dots
-            echo -e "${GREEN}PiercingXX Rice Applied Successfully!${NC}"
-            ;;
-        "Piercing Gimp")
-            # Gimp Dots
-                echo -e "${YELLOW}Installing Piercing Gimp Presets...${NC}"
-                rm -rf gimp-dots
-                if git clone https://github.com/Piercingxx/gimp-dots.git; then
-                    chmod -R u+x gimp-dots
-                    chown -R "$username":"$username" gimp-dots
-                    cd ./gimp-dots || exit
-                    ./gimp-mod.sh
-                    cd "$builddir" || exit
-                    rm -Rf gimp-dots
-                    echo -e "${GREEN}Piercing Gimp Presets Installed Successfully!${NC}"
-                else
-                    echo -e "${RED}Failed to clone gimp-dots repository${NC}"
-                fi
+            echo -e "${GREEN}PiercingXX Gnome Customizations Applied successfully!${NC}"
+            msg_box "System will reboot now."
+            sudo reboot
             ;;
         "Reboot System")
             echo -e "${YELLOW}Rebooting system in 3 seconds...${NC}"
